471618162058d7f0a408a2fadcb0662e6f6c8735
mick17gh <mike17gh@gmail.com>
2021-07-10 08:15:15 +0000

    another change

--- resources/views/backend/access/index.blade.php
+++ resources/views/backend/access/index.blade.php
@@ -5,6 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+
                 <div class="card-header">
                     <button class="btn btn-primary btn-sm" type="button" data-bs-toggle="offcanvas"
                         data-bs-target="#offcanvasRight1" aria-controls="offcanvasRight">
