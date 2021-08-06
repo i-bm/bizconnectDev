43e1ac8cca296a951a1ae31a8f3b7d6931a4d8ad
i-bm <isaac.boakyemanu@gmail.com>
2021-08-06 11:22:42 +0000

    Dashboard Frontend Updates

--- resources/views/backend/theme/index.blade.php
+++ resources/views/backend/theme/index.blade.php
@@ -1,30 +1,58 @@
-@extends('layouts.app')
+@extends('layouts.dashboard.main')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Theme Management') }}
-                    <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary btn-sm pull-right" data-bs-toggle="modal"
-                        data-bs-target="#themeModal">
-                        Add Theme
-                    </button>
-                </div>
+  <!-- Site Content Wrapper -->
+            <div class="dt-content-wrapper">
 
-                <div class="card-body">
+                <!-- Site Content -->
+                <div class="dt-content">
+
+                    <!-- Page Header -->
+                    <div class="dt-page__header">
+                        <h1 class="dt-page__title">Dashboard</h1>
+                    </div>
+                    <!-- /page header -->
+
+                      <!-- Entry Header -->
+                    <div class="dt-entry__header">
+
+                        <!-- Entry Heading -->
+                        <div class="dt-entry__heading">
+                            <h3 class="dt-entry__title">Theme Management</h3>
+                        </div>
+                        <!-- /entry heading -->
+
+                    </div>
+                    <!-- /entry header -->
+
+                    <div class="mb-3 text-right">
+                        <button class="btn btn-primary btn-sm" type="button" data-toggle="modal"
+                            data-target="#addThemeModal" aria-controls="addThemeModal">
+                            Add new theme </button>
+    
+                        {{-- <button class="btn btn-info btn-sm" type="button" data-toggle="modal"
+                            data-target="#createNewPermissionModal" aria-controls="createNewPermissionModal">
+                            Add new permission</button> --}}
+                    </div>
+
+                     <!-- Card -->
+                     <div class="dt-card overflow-hidden">
+
+                        <!-- Card Body -->
+                        <div class="dt-card__body p-0">
+
+                         <!-- Tables -->
+                            <div class="table-responsive">
 
                     <table class="table table-sm">
                         <thead>
                             <tr>
-                                <th scope="col">Theme Name</th>
+                               <th scope="col">Theme Name</th>
                                 <th scope="col">Uploads</th>
                                 <th scope="col">Action</th>
-
                             </tr>
                         </thead>
-                        <tbody style="font-size:12px">
+                       <tbody style="font-size:12px">
                             @foreach($themes as $theme)
                             <tr>
                                 <td>{{$theme->title}}</td>
@@ -60,33 +88,38 @@
 
                         </tbody>
                     </table>
+                    </div>
+                        <!-- /card body -->
+
+                    </div>
+                    <!-- /card -->
+
+</div>  
+                
 
 
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<!----Modals Start-->
 
-
-
-<!-- Modal -->
-<div class="modal fade" id="themeModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+<div class="modal fade" id="addThemeModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
+              <!-- Modal Header -->
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Create New Theme</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h3 class="modal-title">Create New Theme</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
             </div>
-            <form action="{{route('theme.create')}}" method="post" enctype="multipart/form-data">
+            <!-- /modal header -->
+            <form action="{{route('theme.create')}}" method="post">
                 @csrf
                 <div class="modal-body">
-                    <div class="mb-3">
-                        <label class="form-label">Title</label>
+                    <div class="form-group">
+                         <label class="form-label">Title</label>
                         <input type="text" class="form-control form-control-sm" name="title" required>
                     </div>
 
-                    <div class="mb-3">
+                    <div class="form-group">
                         <label class="form-label">Upload Images</label>
                         <input type="file" class="form-control form-control-sm" name="files[]" required multiple
                             accept="image/*">
@@ -94,10 +127,9 @@
 
 
 
-
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary">Save</button>
                 </div>
             </form>
@@ -107,39 +139,71 @@
 
 @foreach($themes as $theme)
 
-<div class="modal fade" id="themeEditModal{{$theme->id}}" tabindex="-1" aria-labelledby="exampleModalLabel"
+<div class="modal fade" id="editPackageModal{{$theme->id}}" tabindex="-1" aria-labelledby="exampleModalLabel"
     aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
+            <!-- Modal Header -->
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Edit Theme details</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h3 class="modal-title">Edit Package Details</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
             </div>
-            <form action="{{route('theme.update', [$theme->id])}}" method="post">
+            <!-- /modal header -->
+            <form action="{{route('package.update', [$package->id])}}" method="post">
                 @csrf
                 @METHOD('PATCH')
                 <div class="modal-body">
-                    <div class="mb-3">
+                    <div class="form-group">
                         <label class="form-label">Title</label>
-                        <input type="text" class="form-control form-control-sm" value="{{$theme->title}}" name="title"
+                        <input type="text" class="form-control form-control-sm" name="title" value="{{$package->title}}"
                             required>
                     </div>
 
-                    <div class="mb-3">
-                        @foreach($theme->themeupload as $image)
-
-                        <img src="{{ asset('storage/files/'.$image->name) }}" width="60" />
-                        @endforeach
-
+                    <div class="form-group">
+                        <label class="form-label">Details <small>(Seperate each item with a comma ',')</small></label>
+                        <textarea class="form-control form-control-sm" rows="3" name="details" required>{{ implode(",", $package->details) }}
+                        </textarea>
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label">Choose Currency</label>
+                        <select class="form-control form-select-sm" name="currency" required>
+                            @if($package->currency == "GHS")
+                            <option>{{$package->currency}}</option>
+                            <option>USD</option>
+                            @endif
+                            @if($package->currency == "USD")
+                            <option>{{$package->currency}}</option>
+                            <option>GHS</option>
+                            @endif
+                        </select>
                     </div>
 
+                    <div class="form-group">
+                        <label class="form-label">Price</label>
+                        <input type="number" class="form-control form-control-sm" value="{{$package->price}}"
+                            placeholder="0.00" name="price" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-label">Duration <small>(months)</small></label>
+                        <input type="number" class="form-control form-control-sm" value="{{$package->duration}}"
+                            name="duration" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-label">Discount % <small>(optional)</small></label>
+                        <input type="number" class="form-control form-control-sm" value="{{$package->discount}}"
+                            name="discount">
+                    </div>
 
 
 
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Save</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save Updates</button>
                 </div>
             </form>
         </div>
@@ -147,24 +211,29 @@
 </div>
 
 
-<div class="modal fade" id="themeDeleteModal{{$theme->id}}" tabindex="-1" aria-labelledby="exampleModalLabel"
+<div class="modal fade" id="deletePackageModal{{$package->id}}" tabindex="-1" aria-labelledby="exampleModalLabel"
     aria-hidden="true">
-    <div class="modal-dialog modal-sm">
+    <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
         <div class="modal-content">
+            <!-- Modal Header -->
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Theme Delete Confirmation</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h3 class="modal-title">Package Delete Confirmation</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
             </div>
-            <form action="{{route('theme.delete', [$theme->id])}}" method="post">
+            <!-- /modal header -->
+            
+            <form action="{{route('package.delete', [$package->id])}}" method="post">
                 @csrf
                 @METHOD('DELETE')
                 <div class="modal-body">
                     <p class="lead">
-                        Are you sure you want to delete Theme? <br>{{$theme->title}}
+                        Are you sure you want to delete Package? <br>{{$package->title}}
                     </p>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary">Yes, Delete now</button>
                 </div>
             </form>
@@ -174,5 +243,10 @@
 
 @endforeach
 
+<!--End Modals-->
+
+</div>
+<!-- /site content -->
 
 @endsection
+
